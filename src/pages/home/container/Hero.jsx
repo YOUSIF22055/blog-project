75fccb4d83bf7709b47d08b0b2131d6693e7706f
YOUSIF22055 <155317301+YOUSIF22055@users.noreply.github.com>
2024-03-07 05:25:40 +0000

    V-5

--- src/pages/home/container/Hero.jsx
+++ src/pages/home/container/Hero.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
+import images from '../../../constants/images'
 
 const Hero = () => {
   return (
-    <section>
-        <div>
-            <h1>Read the most interesting articles</h1>
-            <p>
+    <section className="container mx-auto flex flex-col px-5 py-5 lg:flex-row">
+        <div className="mt-10 lg:w-1/2">
+            <h1 className='font-roboto text-3xl text-center font-bold text-dark-soft md:text-5xl lg:text-left lg:max-w-[540px]'>
+                Read the most interesting articles
+                </h1>
+            <p className='text-dark-light mt-4 text-center md:text-xl lg:text-left'>
                 Lorem ipsum dolor sit amet, consectetur adipscing elit, sed do
                 eiusmod tempor incididunt ut labore et doloer magna aliqua
             </p>
-        </div>
-        <div>
+        <div className='flex flex-col gap-y-2.5 mt-10 relative'>
+            <div>
             <input type="text" />
         </div>
         <button>search</button>
-        <div>
+        </div>
             <div>
                 <span>Popular Tags:</span>
                 <ul>
@@ -24,11 +27,11 @@ const Hero = () => {
                 </ul>
             </div>
         </div>
-        <div>
-            <img src={images} alt="users are reading articles" />
+        <div className="hidden">
+            <img src={images.HeroImage} alt="users are reading articles" />
         </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
