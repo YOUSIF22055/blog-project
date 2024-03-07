38d0014179fdbfaef394bb7f6dced879161ae9d5
YOUSIF22055 <155317301+YOUSIF22055@users.noreply.github.com>
2024-03-07 09:14:55 +0000

    V-5.

--- src/pages/home/container/Hero.jsx
+++ src/pages/home/container/Hero.jsx
@@ -17,15 +17,19 @@ const Hero = () => {
         <div className='flex flex-col gap-y-2.5 mt-10 relative'>
             <div className='relative'>
                 <FiSearch className='absolute left-3 top-1/2 -translate-y-1/2 w-6 h-6 text-[#959EAD]' />
-            <input className='placeholder:font-bold font-semibold text-dark-soft'
+            <input className='placeholder:font-bold font-semibold text-dark-soft placeholder:text-[#959EAD] rounded-lg pl-12 pr-3 w-full py-3 focus:outline-none shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] md:py-4'
              type="text"
             placeholder='Search article'
             />
         </div>
-        <button>search</button>
+        <button className='w-full bg-primary text-whit font-semibold rounded-lg px-5 py-3 md:absolute md:right-2 md:top-1/2 md:-translate-y-1/2 md:w-fit md:py-2'>
+            search
+            </button>
         </div>
-            <div>
-                <span>Popular Tags:</span>
+            <div className='flex mt-4 flex-col lg:flex-row lg:flex-nowrap lg:gap-x-4 lg:mt-7'>
+                <span className='text-dark-light font-semibold italic'>
+                    Popular Tags:
+                    </span>
                 <ul>
                     <li>Design</li>
                     <li>User Experience</li>
