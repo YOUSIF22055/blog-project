a685d4dba238de0ef4e5c676a4f6d56e18a002f2
YOUSIF22055 <155317301+YOUSIF22055@users.noreply.github.com>
2024-03-10 07:08:32 +0000

    V-6

---
+++ src/pages/home/container/Articles.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ArticleCard from "../../../components/ArticleCard";
+
+const Articles = () => {
+    return (
+        <div>
+          <ArticleCard />
+        </div>
+    );
+};
+
+export default Articles
