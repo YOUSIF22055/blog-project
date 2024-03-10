11842733ebc0565bf56afe6c0234f9227f3c63eb
YOUSIF22055 <155317301+YOUSIF22055@users.noreply.github.com>
2024-03-10 09:02:39 +0000

    V-6..

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 
-const ArticleCard = () => {
-    return (
-        <div>
-          
-        </div>
-    )
-}
+import { images } from "../constants";
 
-export default ArticleCard
+const ArticleCard = ({className}) => {
+    return (
+        <div
+         className={`rounded-xl overflow-hidden shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] ${className}`}
+        >
+          <img src={images.Post1Image} alt="title" className="w-full object-cover object-center h-auto" 
+          />
+          <div className="p-5">
+            <h2 className="font-roboto font-bold text-xl txet-dark-soft">
+                Future of Work
+                </h2>
+                <p className="text-dark-light mt-3 text-sm">Majority of peole will work in jobs that don’t exist today.</p>
+                <div className="flex justify-butween flex-nowarp items-center mt-6">
+                    <div className="flex items-center gap-x-2">
+                        <img src={images.PostProfileImage} alt="post profile" />
+                    </div>
+                    <span>02 May</span>
+                </div>
+          </div>
+        </div>
+    );
+};
+
+export default ArticleCard;
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,13 @@
 import Logo from '../assets/Logo.svg';
 import HeroImage from "../assets/HeroImage.svg";
+import Post1Image from "../assets/Posts/Post1.jpg";
+import PostProfileImage from "../assets/Posts/post-prosile.svg";
 
 const images = {
     Logo,
     HeroImage,
+    Post1Image,
+    PostProfileImage,
 };
 
 export default images;
