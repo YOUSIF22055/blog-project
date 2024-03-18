de9d0580b9433243fdd59f9ef521c74bed3d0675
YOUSIF22055 <155317301+YOUSIF22055@users.noreply.github.com>
2024-03-18 08:35:43 +0000

    V-9

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import {Routes, Route, Router} from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/home/HomePage';
 
 function App() {
   return (
     <div className="App font-opensans">
-      <HomePage/>
+      <Routes>
+        <Route index path="/" element={<HomePage/>} />
+      </Routes>
+      
     </div>
   );
 }
