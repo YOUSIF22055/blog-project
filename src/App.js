bfbbd0fb0e330578185a4732be9466c83f4c30de
YOUSIF22055 <155317301+YOUSIF22055@users.noreply.github.com>
2024-04-30 08:09:44 +0000

    24 - Login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import {Toaster} from 'react-hot-toast'
-
 import "./App.css";
 import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
 import HomePage from "./pages/home/HomePage";
 import RegisterPage from "./pages/register/RegisterPage";
+import LoginPage from "./pages/login/loginPage";
+
 function App() {
   return (
     <div className="App font-opensans">
@@ -12,6 +13,7 @@ function App() {
         <Route index path="/" element={<HomePage />} />
         <Route path="/blog/:id" element={<ArticleDetailPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
       <Toaster />
     </div>
