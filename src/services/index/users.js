41e1b46d3e3764106ebbdeb1449e5e89af72277a
YOUSIF22055 <155317301+YOUSIF22055@users.noreply.github.com>
2024-04-30 06:47:28 +0000

    22 - save user data in local storage

--- src/services/index/users.js
+++ src/services/index/users.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-
 export const signup = async ({ name, email, password }) => {
   try {
     const { data } = await axios.post("/api/users/register", {
       name,
       email,
+      password,
     });
     return data;
   } catch (error) {
